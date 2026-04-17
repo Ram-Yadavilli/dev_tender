af6dce7883f5fcb66b46c28370f03b01ed34e955
LakshmanYadavilli <lakshmanyadavilli2001@gmail.com>
2026-04-17 23:18:39 +0530

    changes in app

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 
 app.use("/test", (req, res) => {
-  res.send("Server is Working");
+  res.send("Server is Working...");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
