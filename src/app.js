7fb994f4b5fadfe3173be4c82a7835f4b2406097
LakshmanYadavilli <lakshmanyadavilli2001@gmail.com>
2026-04-20 13:11:51 +0530

    testing http methods

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ Name: "Ram", Age: 22 });
+});
+
+app.post("/user", (req, res) => {
+  res.send(req.body);
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Successfully deleted from DB");
+});
 app.use("/test", (req, res) => {
-  res.send("Server is Working...");
+  res.send("Server is Working on 3000...");
 });
 
 app.listen(3000, () => {
