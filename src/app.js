3c3192ff0f8644b2519dbe6d6d9cedc1c311962e
LakshmanYadavilli <lakshmanyadavilli2001@gmail.com>
2026-04-20 12:20:25 +0530

    testing

--- src/app.js
+++ src/app.js
@@ -5,6 +5,6 @@ app.use("/test", (req, res) => {
   res.send("Server is Working...");
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
